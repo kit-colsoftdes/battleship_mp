077c327625cee625b33b9fc7b9705fc39616e327
Max Fischer <maxfischer2781@gmail.com>
2023-07-10 17:32:42 +0200

    logging and logic fix

--- battleship_mp/server.py
+++ battleship_mp/server.py
@@ -2,12 +2,16 @@ from typing import NamedTuple, Tuple, Iterable, Any, Sequence
 from asyncio import gather, Future, ensure_future, run as run_asyncio
 import argparse
 import random
+import logging
 
 import websockets
 from websockets.server import WebSocketServerProtocol
 
 from .messages import pack, unpack, unpack_keys
 from .exceptions import Deadlock, GameEnd
+
+
+logger = logging.getLogger("battleship_mp.server")
 
 
 def send(_ws: WebSocketServerProtocol, **payload):
@@ -29,12 +33,26 @@ class Game:
     def __init__(self, client_a: Client, client_b: Client):
         self.clients = client_a, client_b
         self.task = ensure_future(self.run())
+        self.identifier = f"{client_a.identifier!r} vs {client_b.identifier!r}"
 
     async def run(self):
-        await self.handle_start()
-        await self.handle_placement()
+        logger.info("run %s", self.identifier)
+        try:
+            await self.handle_start()
+            await self.handle_placement()
+            await self.handle_shots()
+        except GameEnd as ge:
+            logger.info("end %s, winner %s", self.identifier, ge.winner)
+        except Exception as exc:
+            message = pack(error=exc)
+            await gather(
+                self.clients[0].websocket.send(message),
+                self.clients[1].websocket.send(message),
+            )
+            logger.exception("abort %s", self.identifier)
 
     async def handle_start(self):
+        logger.debug("handle_start %s", self.identifier)
         for idx, client in enumerate(self.clients):
             await send(
                 client.websocket,
@@ -43,21 +61,24 @@ class Game:
             )
 
     async def handle_placement(self):
+        logger.debug("handle_placement %s", self.identifier)
         keys = "sizes", "coords", "vertical"
         ships = await gather(
             recv(self.clients[0].websocket, *keys),
             recv(self.clients[1].websocket, *keys),
         )
         await gather(
-            send(self.clients[0].websocket, **ships[1]),
-            send(self.clients[1].websocket, **ships[0]),
+            send(self.clients[0].websocket, **dict(zip(keys, ships[1]))),
+            send(self.clients[1].websocket, **dict(zip(keys, ships[0]))),
         )
 
     async def handle_shots(self):
+        logger.debug("handle_shots %s", self.identifier)
         sock_a, sock_b = self.clients[0].websocket, self.clients[1].websocket
         buffer: "tuple[Any, Any]| None" = None
         while True:
             a_action, b_action = map(unpack, await gather(sock_a.recv(), sock_b.recv()))
+            logger.debug("handle_shots %s, %s, %s", self.identifier, a_action, b_action)
             await self.handle_end(a_action, b_action)
             if "expect_shot" in a_action and "expect_shot" in b_action:
                 if buffer is None:
@@ -78,6 +99,7 @@ class Game:
                 )
             else:
                 buffer = a_action["announce_shot"], b_action["announce_shot"]
+                await gather(send(sock_a), send(sock_b))
 
     async def handle_end(self, a_payload, b_payload):
         if "winner" not in a_payload and "winner" not in b_payload:
@@ -87,16 +109,17 @@ class Game:
             (a_payload, b_payload), self.clients, self.clients[::-1]
         ):
             if payload.get("forfeit") or payload.get("winner") == opponent.identifier:
-                message = pack(error=GameEnd(winner=opponent.identifier))
-                await gather(
-                    client.websocket.send(message), opponent.websocket.send(message)
-                )
+                exc = GameEnd(winner=opponent.identifier)
+                break
         # => players do not agree on who won or no one won
-        message = pack(error=GameEnd(winner=None))
+        else:
+            exc = GameEnd(winner=None)
+        message = pack(error=exc)
         await gather(
             self.clients[0].websocket.send(message),
             self.clients[1].websocket.send(message),
         )
+        raise exc
 
 
 class Server:
@@ -105,12 +128,14 @@ class Server:
         self.wait_start: "Tuple[Client, asyncio.Future[Game]] | None" = None
 
     async def handle_game(self, websocket: WebSocketServerProtocol):
+        logger.debug("handle connection %s", websocket)
         game = await self.create_game(websocket)
         await game.task
 
     async def create_game(self, websocket: WebSocketServerProtocol):
         # wait for the client to start the game
         identifier, version = await recv(websocket, "identifier", "version")
+        logger.info("start %r", identifier)
         # TODO: check version
         # wait for a peer to arrive ...
         if self.wait_start is None:
@@ -130,6 +155,7 @@ class Server:
 async def serve(port: int, hosts: "Sequence[str, ...] | None"):
     server = Server()
     async with websockets.serve(server.handle_game, hosts, port):
+        logging.info("listening on %s of %s", port, hosts)
         await Future()
 
 
@@ -142,5 +168,7 @@ if __name__ == "__main__":
         nargs="*",
         help="addresses/hostnames to bind to [default: all]"
     )
+    CLI.add_argument("--log-level", help="level of log output", default="WARNING")
     args = CLI.parse_args()
+    logging.getLogger("battleship_mp").setLevel(args.log_level.upper())
     run_asyncio(serve(args.PORT, args.ADDRESS))
