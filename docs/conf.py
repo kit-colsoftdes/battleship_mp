3f5061349db7813998b5ef9a30163565cd290678
Max Fischer <maxfischer2781@gmail.com>
2023-07-11 14:29:31 +0200

    Add sphinx documentation (#5)
    * docs: setup basic sphinx and RTD functionality

---
+++ docs/conf.py
@@ -0,0 +1,42 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'BattleShip MP'
+copyright = '2023 Karlsruhe Institute of Technology'
+author = 'CSD Dozenten'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.imgmath',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+intersphinx_mapping = {
+    "python": ('https://docs.python.org/3', None),
+    "websockets": ('https://websockets.readthedocs.io/en/stable/', None),
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+html_theme_options = {
+    "description": "Battleship server/client",
+    'github_user': 'kit-colsoftdes',
+    'github_repo': 'battleship_mp',
+    "fixed_sidebar": True,
+}
---
+++ docs/index.rst
@@ -0,0 +1,21 @@
+.. battleship_mp documentation master file, created by
+   sphinx-quickstart on Tue Jul 11 09:26:28 2023.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+KIT CSD Battleship MP documentation
+===================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+The :py:mod:`battleship_mp` provides a client/server
+for use as part of the KIT course Collaborative Software Design.
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
