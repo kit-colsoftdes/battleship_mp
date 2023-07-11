9a477ef1080a33a9075a344483314f4a5311bd5a
Max Fischer <maxfischer2781@gmail.com>
2023-07-11 09:56:24 +0200

    prepared configuration

--- docs/conf.py
+++ docs/conf.py
@@ -7,21 +7,36 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = 'BattleShip MP'
-copyright = '2023, CSD Dozenten'
+copyright = '2023 Karlsruhe Institute of Technology'
 author = 'CSD Dozenten'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.imgmath',
+    'sphinx.ext.viewcode',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+intersphinx_mapping = {
+    "python": ('https://docs.python.org/3', None),
+    "websockets": ('https://websockets.readthedocs.io/en/stable/', None),
+}
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
+html_theme_options = {
+    "description": "Battleship server/client",
+    'github_user': 'kit-colsoftdes',
+    'github_repo': 'battleship_mp',
+    "fixed_sidebar": True,
+}
